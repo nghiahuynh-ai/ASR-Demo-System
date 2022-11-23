db83f069f2e9776ad72be41c47baa890bc40635a
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-23 16:57:56 +0700

    fix onclickevent.js

--- static/js/onclickevent.js
+++ static/js/onclickevent.js
@@ -1,56 +1,54 @@
-let recorder;
-let chunks;
+URL = window.URL || window.webkitURL;
+
+let gumStream;                      //stream from getUserMedia()
+let rec;                            //Recorder.js object
+let input;                          //MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let audioContext //audio context to help us record
 
 function toggle(element) {
     if (element.innerHTML == 'Record') {
         element.style.backgroundColor = 'red';
         element.innerHTML = 'Stop';
         
-        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-        chunks = [];
-        recorder = new MediaRecorder(stream);
-        recorder.ondataavailable = e => {
-            chunks.push(e.data);
-        };
-        recorder.start(0);
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(function(stream) {
+            console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+            audioContext = new AudioContext();
+            gumStream = stream;
+            input = audioContext.createMediaStreamSource(stream);
+            rec = new Recorder(input,{numChannels:1})
+            rec.record()
+            console.log("Recording started");
+    
         }).catch(console.error);
     }
     else {
         element.style.backgroundColor = '#008CBA';
         element.innerHTML = 'Record'
-        recorder.stop()
-        let file = new File(chunks, 'mysound.wav', {type: 'audio/wav'});
-        const formData = new FormData();
-        formData.append('file', file);
-        axios({
-            method: "post",
-            url: "http://127.0.0.1:5000/",
-            data: formData,
-            headers: { "Content-Type": "multipart/form-data" },
-          })
-            .then(function (response) {
-                console.log(response);
-                const resultElement = document.getElementById('result');
-                resultElement.innerHTML = response.data.cfm.transcript
-            })
-            .catch(function (response) {
-              console.log(response);
-            });
+
+        rec.stop();
+        gumStream.getAudioTracks()[0].stop();
+        rec.exportWAV(createDownloadLink);
     }
 }
 
-function createAudioElement(blobUrl) {
-    const downloadEl = document.createElement('a');
-    downloadEl.style = 'display: block';
-    downloadEl.innerHTML = 'download';
-    downloadEl.download = 'audio.wav';
-    downloadEl.href = blobUrl;
-    const audioEl = document.createElement('audio');
-    audioEl.controls = true;
-    const sourceEl = document.createElement('source');
-    sourceEl.src = blobUrl;
-    sourceEl.type = 'audio/wav';
-    audioEl.appendChild(sourceEl);
-    document.body.appendChild(audioEl);
-    document.body.appendChild(downloadEl);
+function createDownloadLink(blob) {
+    let xhr=new XMLHttpRequest();
+        xhr.onload=function(e) {
+              if(this.readyState === 4) {
+                  console.log("Server returned: ",e.target.responseText);
+              }
+        };
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("result").innerHTML =
+                this.responseText;
+           }
+        };
+          let fd=new FormData();
+          fd.append("audio_data",blob, 'audio.wav');
+          xhr.open("POST","/",true);
+          xhr.send(fd);
 }
